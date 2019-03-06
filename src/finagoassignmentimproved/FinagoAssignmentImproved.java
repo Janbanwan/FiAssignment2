38574b5648e494b028faac77e288b4b3e9fd607d
janban <janne.antila@hotmail.com>
2019-03-06 16:25:25 +0000

    added comments

--- src/finagoassignmentimproved/FinagoAssignmentImproved.java
+++ src/finagoassignmentimproved/FinagoAssignmentImproved.java
@@ -21,19 +21,22 @@ import org.xml.sax.SAXException;
 public class FinagoAssignmentImproved {
 
     public static void main(String[] args) {
-        
+
         String templateName = "CSVTemplate.xsl";
         String inputXML = "exampleInvoice.xml";
         String outputFileName = "Finvoice.csv";
         
         try {
+            //Loading the template for the csv as well as the input XML the application
             File stylesheet = new File(templateName);
             File input = new File(inputXML);
             
+            //Creating a document object and parsing the input from the XML file to the document object
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document document = builder.parse(input);
             
+            //Applying the xsl template to the xml to create a CSV file in the specified format
             StreamSource stylesource = new StreamSource(stylesheet);
             Transformer transformer = TransformerFactory.newInstance()
                     .newTransformer(stylesource);
